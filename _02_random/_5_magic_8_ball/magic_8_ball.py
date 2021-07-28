29f149e22b8fd5e47b40e1d271c6db5902ce00b7
caris <yeahredpanda@gmail.com>
2021-07-28 01:31:41 +0000

    magic 8 ball

--- _02_random/_5_magic_8_ball/magic_8_ball.py
+++ _02_random/_5_magic_8_ball/magic_8_ball.py
@@ -6,21 +6,33 @@ if __name__ == '__main__':
     window.withdraw()
 
     # TODO Get the user to enter a question for the 8 ball to answer
+    simpledialog.askstring("Enter a question for the magic 8-ball to answer")
 
     # Make a variable and initialize it to a random number between 0 and 3
+    number = random.randint(0,3)
+    print(number)
+
 
     # If the random number is 0
+    if number == 0:
+        print("Yes")
 
         # tell the user "Yes"
 
     # If the random number is 1
+    if number == 1:
+        print("No")
 
         # tell the user "No"
 
     # If the random number is 2
+    if number == 2:
+        print("Maybe you should ask Google?")
 
         # tell the user "Maybe you should ask Google?"
 
     # If the random number is 3
+    if number == 3:
+        print("Yes No Maybe So")
 
         # write your own answer
